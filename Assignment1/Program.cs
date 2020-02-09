a883778694d6a2654ffa2971c22c99be791dec9a
Srinivasa Arudra Phani Bhushan Kolla <bhushankolla@usf.edu>
2020-02-09 16:20:12 -0500

    completed till question 6 with comments

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -8,24 +8,35 @@ namespace Assignment1_Spring2020
     {
         static void Main(string[] args)
         {
-            int n = 0;
-          // PrintPattern(n);
+            Console.WriteLine("Solution 1");
+            int n = 5;
+            PrintPattern(n);
 
+            Console.WriteLine("\n");
+            Console.WriteLine("Solution 2");
             int n2 = 6;
-           //PrintSeries(n2);
+            PrintSeries(n2);
 
+            Console.WriteLine("\n");
+            Console.WriteLine("Solution 3");
             string s = "09:15:35PM";
-            //string t = UsfTime(s);
-            //Console.WriteLine(t);
+            string t = UsfTime(s);
+            Console.WriteLine(t);
 
+            Console.WriteLine("\n");
+            Console.WriteLine("Solution 4");
             int n3 = 110;
             int k = 11;
-           // UsfNumbers(n3, k);
+            UsfNumbers(n3, k);
 
+            Console.WriteLine("\n");
+            Console.WriteLine("Solution 5");   
             string[] words = new string[] {"abcd", "dcba", "lls", "s", "sssll"};
-            //PalindromePairs(words);
+            PalindromePairs(words);
 
-            int n4 = 31;
+            Console.WriteLine("\n");
+            Console.WriteLine("Solution 6");
+            int n4 = 5;
             Stones(n4);
         }
 
@@ -34,8 +45,9 @@ namespace Assignment1_Spring2020
         {
             try
             {
-                 
-                //checking for n,because n cannot be less than or equal to 0.                
+                if (n > 0)
+                {
+                    //checking for n,because n cannot be less than or equal to 0.                
                     if (n == 0)
                         return;
                     //calling Printn method.
@@ -44,7 +56,11 @@ namespace Assignment1_Spring2020
 
                     // recursively calling PrintPattern() 
                     PrintPattern(n - 1);
-                
+                }
+                else
+                {
+                    Console.WriteLine("Input should be greater than 0, please enter a valid input");
+                }
             }
             catch(Exception e)
             {
@@ -53,6 +69,7 @@ namespace Assignment1_Spring2020
             }
         }
 
+        //second recursive function for printing elements in a row.
         private static void Printn(int n)
         {
             if (n == 0)
@@ -62,20 +79,28 @@ namespace Assignment1_Spring2020
             Printn(n - 1);
         }
 
+        
         private static void PrintSeries(int n2)
         {
             try
             {
-                int sum = 0;
-                for (int i = 1; i <= n2; i++)
+                if (n2 > 0)
                 {
-                    //adding the sum to the next integer.
-                    sum = sum + i;
-                    if (i != n2)
-                        Console.Write(sum + ",");
-                    //removing coma for the last number.
-                    else
-                        Console.Write(sum);
+                    int sum = 0;
+                    for (int i = 1; i <= n2; i++)
+                    {
+                        //adding the sum to the next integer.
+                        sum = sum + i;
+                        if (i != n2)
+                            Console.Write(sum + ",");
+                        //removing coma for the last number.
+                        else
+                            Console.Write(sum);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input should be greater than 0");
                 }
             }
             //Handling exception and printing the error message.
@@ -119,31 +144,37 @@ namespace Assignment1_Spring2020
         {
             try
             {
-               
-                for(int i = 1; i<=n3; i++)
+                if (n3 != 0 && k != 0)
                 {
-                    //Here we are checking for the 3 and 5 condition first so that it will not print for i%3==0 or i%5==0
-                    if (i % 3 == 0 && i % 5 == 0)
-                        Console.Write("US ");
-                    //Here we are checking for the 5 and 7 condition first so that it will not print for i%5==0 or i%7==0
-                    else if (i % 5 == 0 && i % 7 == 0)
-                        Console.Write("SF ");
-                    //checking for multiples of 3
-                    else if (i % 3 == 0)
-                        Console.Write("U ");
-                    //checking for multiples of 5
-                    else if (i % 5 == 0)
-                        Console.Write("S ");
-                    //checking for multiples of 7
-                    else if (i % 7 == 0)
-                        Console.Write("F ");
-                    //printing the number itself if it not a multiple of any of the above.
-                    else
-                        Console.Write(i+" ");
+                    for (int i = 1; i <= n3; i++)
+                    {
+                        //Here we are checking for the 3 and 5 condition first so that it will not print for i%3==0 or i%5==0
+                        if (i % 3 == 0 && i % 5 == 0)
+                            Console.Write("US ");
+                        //Here we are checking for the 5 and 7 condition first so that it will not print for i%5==0 or i%7==0
+                        else if (i % 5 == 0 && i % 7 == 0)
+                            Console.Write("SF ");
+                        //checking for multiples of 3
+                        else if (i % 3 == 0)
+                            Console.Write("U ");
+                        //checking for multiples of 5
+                        else if (i % 5 == 0)
+                            Console.Write("S ");
+                        //checking for multiples of 7
+                        else if (i % 7 == 0)
+                            Console.Write("F ");
+                        //printing the number itself if it not a multiple of any of the above.
+                        else
+                            Console.Write(i + " ");
 
-                    //Starting a new line after k elements.
-                    if(i%k==0)
-                        Console.WriteLine();
+                        //Starting a new line after k elements.
+                        if (i % k == 0)
+                            Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid input so that n3 and k should be greater than 0");
                 }
             }
             catch(Exception e)
@@ -158,56 +189,63 @@ namespace Assignment1_Spring2020
         {
             try
             {
-                //initiating an empty list of type integer array to store the index values of palindrome strings.
-                List<int[]> result = new List<int[]>();
-                //initiating an empty string to print the result in required format. 
-                string s = "";
-                //used nested for loop to try all the possible combinations in the input string array.
-                for (int i = 0; i < words.Length; i++)
+                //checking for number of strings in the strings array.
+                if (words.Length > 1)
                 {
-                    for (int j = 0; j < words.Length; j++)
+                    //initiating an empty list of type integer array to store the index values of palindrome strings.
+                    List<int[]> result = new List<int[]>();
+                    //initiating an empty string to print the result in required format. 
+                    string s = "";
+                    //used nested for loop to try all the possible combinations in the input string array.
+                    for (int i = 0; i < words.Length; i++)
                     {
-                        //checking for i!=j so to remove combinations of same array.
-                        if(i!=j)
+                        for (int j = 0; j < words.Length; j++)
                         {
-                            //forming a temporary string for each combination
-                            string temp = words[i] + words[j];
-                            //flag variable to check for palindrome
-                            int pal = 1;
-                            //checking for palindrome condition.
-                            for (int k = 0;  k < temp.Length/2; k++)
+                            //checking for i!=j so to remove combinations of same array.
+                            if (i != j)
                             {
-                                if (temp[k] != temp[temp.Length - k - 1])
+                                //forming a temporary string for each combination
+                                string temp = words[i] + words[j];
+                                //flag variable to check for palindrome
+                                int pal = 1;
+                                //checking for palindrome condition.
+                                for (int k = 0; k < temp.Length / 2; k++)
                                 {
-                                    //making flag variable 0 if the string is not a palindrom and breaking from the loop.
-                                    pal = 0;
-                                    break;
+                                    if (temp[k] != temp[temp.Length - k - 1])
+                                    {
+                                        //making flag variable 0 if the string is not a palindrom and breaking from the loop.
+                                        pal = 0;
+                                        break;
+                                    }
+
                                 }
-                                
+                                //if value of flag is 1, then the temp string is a palindrom, as it did not become 0.
+                                if (pal == 1)
+                                {
+                                    //storing the values of i and j in a list of type of int array.
+                                    int[] tempArray = { i, j };
+                                    result.Add(tempArray);
+                                    //adding value of i and j to the string 
+                                    s = s + "[" + i + "," + j + "]" + ",";
+                                }
                             }
-                            //if value of flag is 1, then the temp string is a palindrom, as it did not become 0.
-                            if (pal == 1)
-                            {
-                                //storing the values of i and j in a list of type of int array.
-                                int[] temparray = {i,j};
-                                result.Add(temparray);
-                                //adding value of i and j to the string 
-                                s = s + "[" + i + "," + j + "]" + ",";
-                            }
+
+
                         }
-
-
                     }
+                    //printing the output in the required format.
+                    Console.Write("[");
+                    //removing the comma at the end of the string.
+                    for (int l = 0; l < s.Length - 1; l++)
+                    {
+                        Console.Write(s[l]);
+                    }
+                    Console.Write("]");
                 }
-                //printing the output in the required format.
-                Console.Write("[");
-                // removing the comma at the end of the string.
-                for (int l = 0; l < s.Length-1; l++)
+                else
                 {
-                    Console.Write(s[l]);
+                    Console.WriteLine("Minimum number of strings should be 2, please enter a valid input.");
                 }
-                Console.Write("]");
-                     
             }
             catch (Exception e)
             {
@@ -219,18 +257,31 @@ namespace Assignment1_Spring2020
         {
             try
             {
+                //for a game to happen, both the players should get a chance, so minimum number of stones should be 4.
                 if (n4 >= 4)
                 {
                     string stonesResult = "[";
+                    //Player 2 will win when the number of stones is 4 because if player 1 takes 1 or 2 or 3 stones
+                    // player 2 will take out the reamining balls and he will win. Similary if the number of balls is a multiple
+                    // of 4, player 2 will win, so we are printing false.
+                
                     if (n4 % 4 == 0)
-                        Console.WriteLine("false"); 
-                    else 
+                        Console.WriteLine("false");
+                //if the number of stones is 5, player 1 will take out 1 stone, so that player2 is left with 4 stones to pick from, so Player 1 will win.
+                //if the number of stones is 6, player 1 will take out 2 stones, so that Player2 is left with 4 stones to pick from, so Player 1 will win.
+                //if the number of stones is 7, player 1 will take out 3 stones, so that Player2 is left with 4 stones to pick from, so Player 1 will win.
+                //so similary for a large number of stones, player 1 will pick n%4 number of stones, so Player 2 will be left with stones that is a multiple of 4 and player1 will win.
+
+                    else
                     {
-                            stonesResult = stonesResult + n4 % 4;
+                        //player 1 will pick n4%4 stones first to win.
+                        stonesResult = stonesResult + n4 % 4;
                         for (int i = 0; i < n4/4; i++)
                         {
+                            //1,3 is one of the combination, it can be 2,2 or 3,1. We are printing the 1,3 combination here.
                             stonesResult = stonesResult+ " 1 3";
                         }
+                        
                         stonesResult = stonesResult + "]";
                             Console.WriteLine(stonesResult);
                     }
